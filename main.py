d8eb18a9a147bcc95ed3bb18573572ac34cdc983
Najeh <najeh.ddembe@bcestudent.com>
2026-01-06 14:02:26 +0000

    Started work on GUI

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import matplotlib as mpl
 import pandas as pd
 import numpy as np
-import tkinter as tk
+import dearpygui.dearpygui as dpg
 
-root = tk.Tk()
-root.mainloop()
+dpg.create_context()
+dpg.create_viewport(title="ZDex", width=1280, height=720)
+dpg.set_viewport_min_width(854)
+dpg.set_viewport_min_height(480)
+dpg.set_viewport_large_icon('data/Images/GreatBall.ico')
+
+with dpg.window(label="Welcome"):
+    dpg.add_text("Welcome to ZDex!")
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
+
